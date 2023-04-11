305f6c422d2805124194e072bcb91995d2be5722
YohanToledo <yohan.gabriel.bernart@gmail.com>
2023-04-11 17:53:25 -0300

    fix: change status code at login method from 201 to 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ResponseAndReturnTypes } from 'src/helper/swagger.helper';
@@ -9,12 +9,13 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiOkResponse({ type: ResponseAndReturnTypes.JwtExampleRes })
+  @ApiOkResponse({ type: ResponseAndReturnTypes.JwtExampleRes})
   @ApiBody({ type: ResponseAndReturnTypes.LoginExampleReq })
   @ApiOperation({
     summary: 'Validate user and return jwt access token',
   })
   @UseGuards(AuthGuard('local'))
+  @HttpCode(200)
   @Post('login')
   async login(@Req() req: any) {
     return await this.authService.login(req.user);
